84241dfb9364aacc2fe114fd0f339f3ce7f09893
Nikhil Atkinson <nikhilatkinson1@gmail.com>
2025-11-10 22:07:37 +1100

    fixing s3 infra

--- terraform/s3/main.tf
+++ terraform/s3/main.tf
@@ -18,28 +18,6 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
-resource "aws_s3_bucket" "terraform-mnist-state041125" {
-    bucket = "terraform-mnist-state041125"
-    force_destroy = true
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "terraform-mnist-state041125-encryption" {
-    bucket = aws_s3_bucket.terraform-mnist-state041125.id
-
-    rule {
-        apply_server_side_encryption_by_default {
-            sse_algorithm = "AES256"
-        }
-    }
-}
-
-resource "aws_s3_bucket_versioning" "terraform-mnist-state041125-versioning" {
-    bucket = aws_s3_bucket.terraform-mnist-state041125.id
-    versioning_configuration {
-        status = "Enabled"
-    }
-}
-
 resource "aws_s3_bucket" "mnist-model-bucket09112025" {
     bucket = "mnist-model-bucket-09112025"
     force_destroy = true
